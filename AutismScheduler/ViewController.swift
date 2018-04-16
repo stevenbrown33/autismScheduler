06ef9d37edf4551a7f96c6ea396003d2c6fdea64
stevenbrown33 <stevebrown33@me.com>
2018-04-16 09:38:00 -0600

    Initial Commit

---
+++ AutismScheduler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutismScheduler
+//
+//  Created by Steven Brown on 4/16/18.
+//  Copyright © 2018 Steven Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
