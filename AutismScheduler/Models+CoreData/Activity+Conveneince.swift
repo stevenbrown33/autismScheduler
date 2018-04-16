872b33b9e2d8cd46d8f0ee355ac1143f33cbbac2
stevenbrown33 <stevebrown33@me.com>
2018-04-16 10:22:50 -0600

    view hierarchy, setup coredata, setup cloudkit

---
+++ AutismScheduler/Models+CoreData/Activity+Conveneince.swift
@@ -0,0 +1,9 @@
+//
+//  Activity+Conveneince.swift
+//  AutismScheduler
+//
+//  Created by Steven Brown on 4/16/18.
+//  Copyright © 2018 Steven Brown. All rights reserved.
+//
+
+import Foundation
